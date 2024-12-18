79954695c7c64c43c79cf3f87e359d145f0b5bce
Christopher Steigerwald <cjsteigerwald@gmail.com>
2024-12-18 10:20:53 -0800

    using secrets

--- data/database.js
+++ data/database.js
@@ -4,13 +4,8 @@ const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
-// const clusterAddress = 'cluster0.coqsd.mongodb.net';
-// const dbUser = 'mongodb-user';
-// const dbPassword = 'LCp99gTYh4lXsJUW';
-// const dbName = 'gha-demo';
 
 const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
-// const uri = `mongodb+srv://cjsteigerwald:pGWelvHSZxIlrJSf@cluster0.coqsd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
