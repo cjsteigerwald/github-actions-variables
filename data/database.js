71089e4f45b6d9cea1bb4a354083acd7f8dd1e91
Christopher Steigerwald <cjsteigerwald@gmail.com>
2024-12-18 09:38:08 -0800

    test

--- data/database.js
+++ data/database.js
@@ -5,19 +5,20 @@ const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.coqsd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
 
 try {
-  await client.connect();
-  await client.db(dbName).command({ ping: 1 });
-  console.log('Connected successfully to server');
+	await client.connect();
+	await client.db(dbName).command({ ping: 1 });
+	console.log('Connected successfully to server');
 } catch (error) {
-  console.log('Connection failed.');
-  await client.close();
-  console.log('Connection closed.');
+	console.log('Connection failed.');
+	await client.close();
+	console.log('Connection closed.');
 }
 
 const database = client.db(dbName);
